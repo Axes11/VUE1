7e790857b03f0077c3c74634d87ad34abdcb5ed0
Axes11 <you@example.com>
2025-03-16 18:33:03 +0100

    reg & login layout

---
+++ seabattle/src/components/Header.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+  import DesignShip1 from "@/components/icons/DesignShip1.vue";
+  import DesignShip2 from "@/components/icons/DesignShip2.vue";
+  import Logo from "@/components/icons/Logo.vue";
+</script>
+
+<template>
+  <div class="w-full flex justify-center">
+    <div class="flex gap-5">
+      <DesignShip1 class="ship"/>
+      <Logo class="logo"/>
+      <DesignShip2 class="ship"/>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  .ship{
+    width: 50px;
+    height: 40px;
+  }
+  .logo{
+    width: 160px;
+    height: 40px;
+  }
+</style>
--- seabattle/src/views/Registration.vue
+++ seabattle/src/views/Registration.vue
@@ -1,22 +1,47 @@
 <script setup lang="ts">
-
-import DesignShip1 from "@/components/icons/DesignShip1.vue";
-import DesignShip2 from "@/components/icons/DesignShip2.vue";
-import Logo from "@/components/icons/Logo.vue";
+  import DesignShip1 from "@/components/icons/DesignShip1.vue";
+  import DesignShip2 from "@/components/icons/DesignShip2.vue";
+  import Header from "@/components/Header.vue";
 </script>
 
 <template>
+  <Header/>
   <div class="h-screen flex items-center justify-center">
-    <div class="wrapper">
-
+    <div class="wrapper flex justify-between p-8">
+      <div class="registration flex flex-col items-center justify-between gap-5">
+        <DesignShip1 class="ship"/>
+        <div class="flex flex-col gap-3">
+          <input class="input" placeholder="NAME..." type="text" name="" id="">
+          <input class="input" placeholder="E-MAIL..." type="text" name="" id="">
+          <input class="input" placeholder="PASSWORD..." type="text" name="" id="">
+        </div>
+        <button class="button w-full">REGISTER</button>
+      </div>
+      <div class="login flex flex-col items-center justify-between">
+        <DesignShip2 class="ship"/>
+        <div class="flex flex-col gap-3">
+          <input class="input" placeholder="NAME..." type="text" name="" id="">
+          <input class="input" placeholder="PASSWORD..." type="text" name="" id="">
+        </div>
+        <button class="button w-full">LOGIN</button>
+      </div>
     </div>
   </div>
 </template>
 
 <style scoped>
-  .wrapper{
-    width: 638px;
-    height: 402px;
+  .wrapper {
     background-image: url("/RegBg.png");
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    border-radius: 30px;
+    width: auto;
+    height: auto;
+    gap: 50px;
+  }
+  .ship{
+    width: 120px;
+    height: 100px;
   }
 </style>
